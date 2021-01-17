4f632b316dcd6ad5ff7feafe4e0e81a2972e9db4
Arda Serim <arserim20@gmail.com>
2021-01-17 13:45:53 +0300

    Added Camera Fixer
    Camera now adapts different aspect ratios with respect to width instead of height by changing field of view

---
+++ Assets/Scripts/CameraFixer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraFixer : MonoBehaviour
+{
+	private static float fixedFov = 60 / (16.0f / 9); 
+
+	void Start()
+	{
+		// Force fixed width
+		float ratio = (float)Screen.height / (float)Screen.width;
+		GetComponent<CinemachineVirtualCamera>().m_Lens.FieldOfView = ratio * fixedFov;
+	}
+}
