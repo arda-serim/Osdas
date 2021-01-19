8e76db13788454be7cffbee26ed2b9ad993e0454
Berkin Serim <brknsrm0@gmail.com>
2021-01-19 18:03:16 +0300

    Osdas is behaving as intending.
    Stamina system added. Osdas' animation blends are good, camera effects added for diving. Also camera borders added.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Player : MonoBehaviour
 {
@@ -9,10 +11,14 @@ public class Player : MonoBehaviour
 
     Animator animator;
     Rigidbody rb;
+    CinemachineFramingTransposer cinemachine;
+
+    bool isDiving;
 
     float speed = 2;
+    [SerializeField]float stamina = 100;
 
-    CinemachineFramingTransposer cinemachine;
+    Vignette vignette;
 
     void Awake()
     {
@@ -21,6 +27,10 @@ public class Player : MonoBehaviour
         animator = this.gameObject.GetComponent<Animator>();
         rb = this.gameObject.GetComponent<Rigidbody>();
 
+        isDiving = false;
+
+        GameObject.Find("Global Volume").GetComponent<Volume>().profile.TryGet(out vignette);
+
         #region Inputs
         playerControls = new PlayerControls();
 
@@ -28,35 +38,101 @@ public class Player : MonoBehaviour
 
         playerControls.Gameplay.Dive.performed += ctx => 
         {
-            animator.SetBool("IsDiving", true);
-            rb.velocity = Vector3.down * speed * 4;
-            cinemachine.m_SoftZoneHeight = 0.25f;
+            isDiving = true;
         };
         playerControls.Gameplay.Dive.canceled += ctx =>
         {
-            animator.SetBool("IsDiving", false);
-            rb.velocity = Vector3.down * speed;
-            StartCoroutine(CinemachineNormaliser());
+            isDiving = false;
         };
         #endregion
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
-    }
-
-    IEnumerator CinemachineNormaliser()
-    {
-        while (cinemachine.m_SoftZoneHeight > 0)
+        if (stamina <= 0)
         {
-            if (playerControls.Gameplay.Dive.phase == UnityEngine.InputSystem.InputActionPhase.Performed)
-            {
-                break;
-            }
-            cinemachine.m_SoftZoneHeight -= 0.003f;
-            yield return new WaitForEndOfFrame();
+            isDiving = false;
         }
 
+        if (isDiving && stamina > 0)
+        {
+            animator.SetBool("IsDiving", true);
+            rb.velocity = Vector3.down * speed * 4;
+            cinemachine.m_SoftZoneHeight = 0.35f;
+            FOVIncreaser();
+            VigniteIncreaser();
+            StaminaReducer();
+        }
+
+        if (!isDiving)
+        {
+            animator.SetBool("IsDiving", false);
+            rb.velocity = Vector3.down * speed;
+            CinemachineNormaliser();
+            FOVNormaliser();
+            VigniteNormaliser();
+            StaminaNormaliser();
+        }
+    }
+
+    void CinemachineNormaliser()
+    {
+        if (cinemachine.m_SoftZoneHeight > 0)
+        {
+            cinemachine.m_SoftZoneHeight -= 0.009f;
+        }
+
+    }
+    
+    void FOVIncreaser()
+    {
+        if (Camera.main.GetComponentInChildren<CinemachineVirtualCamera>().m_Lens
+            .FieldOfView < 70)
+        {
+            Camera.main.GetComponentInChildren<CinemachineVirtualCamera>().m_Lens
+            .FieldOfView += 0.35f;
+        }
+    }    
+    
+    void FOVNormaliser()
+    {
+        if (Camera.main.GetComponentInChildren<CinemachineVirtualCamera>().m_Lens
+            .FieldOfView > 60)
+        {
+            Camera.main.GetComponentInChildren<CinemachineVirtualCamera>().m_Lens
+            .FieldOfView -= 0.3f;
+        }
+    }    
+    
+    void VigniteIncreaser()
+    {
+        if (vignette.intensity.value < 0.15)
+        {
+            vignette.intensity.value += 0.003f;
+        }
+    }   
+    
+    void VigniteNormaliser()
+    {
+        if (vignette.intensity.value > 0)
+        {
+            vignette.intensity.value -= 0.01f;
+        }
+    }
+
+    void StaminaReducer()
+    {
+        if (stamina > 0)
+        {
+            stamina -= 1f;
+        };
+    }
+
+    void StaminaNormaliser()
+    {
+        if (stamina < 100)
+        {
+            stamina += 1f;
+        }
     }
 }
