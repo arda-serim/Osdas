eb781022e2b73789a7fd0915f4cc4c49a0a56ac0
Arda Serim <arserim20@gmail.com>
2021-01-20 16:01:01 +0300

    SpawnManager

---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T> 
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this as T;
+    }
+}
